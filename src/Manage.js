333bfeeae392875bf6548da00dfd409044c37be5
norths125 <norths233@gmail.com>
2022-11-28 23:49:45 +0700

    ge

--- src/Manage.js
+++ src/Manage.js
@@ -1,4 +1,4 @@
-import React ,{useEffect} from 'react';
+import React ,{useState,useEffect} from 'react';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
@@ -16,13 +16,16 @@ import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {Routes, Route, useNavigate} from 'react-router-dom';
 import { Home } from '@mui/icons-material';
-import Header from './component/Header';
+import ModalAddUser from './dia';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9,10];
 
 const theme = createTheme();
 
 export default function Album() {
+  const [open,setOpen]=useState(false)
     useEffect(() => {
         const token = localStorage.getItem('token')
         fetch('http://localhost:3333/authen', {
@@ -66,15 +69,50 @@ export default function Album() {
     const HOME = () => {
       navigate('/Home');
     };
+    const Managegetid = () => {
+      // navigate('/Managegetid');
+      setOpen(true)
+    };
 
   return (
     <ThemeProvider theme={theme}>
+      {open && 
+        <ModalAddUser open={open} setOpen={setOpen}/>
+      }
       <CssBaseline />
       <Header />
       <main>
-        <Container sx={{ py: 0 }} maxWidth="md">
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',pt: 8,pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1" variant="h2"
+              align="center" color="text.primary"
+              gutterBottom
+            >
+              Kiosk Manage
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Coming Soon!
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Kiosk</Button>
+              <Button variant="outlined">EXAT</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
-          <Grid container spacing={5}>
+          <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
@@ -98,7 +136,8 @@ export default function Album() {
                     </Typography>
                   </CardContent>
                   <CardActions>
-                    <Button size="small">View</Button>
+                    {/* <Button variant="variant" color="inherit" onClick={Managegetid}>Managegetid</Button> */}
+                    <Button size="small" onClick={Managegetid}>View</Button>
                     <Button size="small">Edit</Button>
                   </CardActions>
                 </Card>
@@ -107,7 +146,6 @@ export default function Album() {
           </Grid>
         </Container>
       </main>
-
     </ThemeProvider>
   );
 }
